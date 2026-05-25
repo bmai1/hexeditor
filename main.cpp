06390b1241b75e44a7b5db382db641c0860a2090
Brian Mai <0hrs3g@gmail.com>
2026-05-24 18:47:29 -0700

    Highlight hex/ascii with rectangles on click

--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@ static void SDLCALL callback(void* userdata, const char* const* filelist, int fi
     bytes.assign((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
 }
 
+// Format bytes into Offset Hex ASCII string
 std::string format_row(const std::vector<unsigned char>& bytes, size_t offset) {
     std::ostringstream oss;
 
@@ -64,6 +65,26 @@ std::string format_row(const std::vector<unsigned char>& bytes, size_t offset) {
     return oss.str();
 }
 
+// Get byte index based on where the mouse clicks on the window
+size_t get_byte_index(int mouse_x, int mouse_y)
+{
+    constexpr int TOP_OFFSET = 34;
+    constexpr int ROW_H = 16;
+
+    constexpr int HEX_START_X = 110;
+    constexpr int HEX_CELL_W = 30;
+
+    int row = (mouse_y - TOP_OFFSET) / ROW_H;
+    int col = (mouse_x - HEX_START_X) / HEX_CELL_W;
+
+    if (row < 0 || col < 0 || col >= 16)
+        return (size_t)-1;
+
+    size_t index = row * 16 + col;
+
+    return index;
+}
+
 int main(int argc, char* argv[]) {
     SDL_Window *window;
     SDL_Renderer *renderer;            
@@ -78,6 +99,8 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND); // for opacity
+
     // Init font
     TTF_Init();
     TTF_Font* font = TTF_OpenFont("SpaceMono.ttf", 16);
@@ -86,15 +109,44 @@ int main(int argc, char* argv[]) {
     SDL_ShowOpenFileDialog(callback, NULL, window, NULL, 0, NULL, false);
 
     bool running = true;
+    bool showCursorRect = true;
+
+    SDL_FRect cursorRect;
+    SDL_FRect asciiRect;
 
     while (running) {
         SDL_Delay(50); // decrease fps 
 
         SDL_Event event;
-        SDL_PollEvent(&event);
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT)
                 running = false;
+            else if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    float click_x = event.button.x;
+                    float click_y = event.button.y;
+                    size_t byte_index = get_byte_index((int) click_x, (int) click_y);
+
+                    if (byte_index != (size_t)-1) {
+                        SDL_Log("Mouse left-clicked at: %f, %f, which corresponds to byte index %zu", click_x, click_y, byte_index);
+                        // Calculated based on offsets in get_byte_index
+                        cursorRect = {(float) 110+30*(byte_index%16), (float) 31+16*(byte_index/16), 20, 14};
+                        asciiRect  = {(float) 600+10*(byte_index%16), (float) 31+16*(byte_index/16), 10, 14};
+                        if (!showCursorRect) {
+                            showCursorRect = true;
+                        }
+                        else {
+                            // showCursorRect = false;
+                        }
+                    }
+                    else {
+                        SDL_Log("Mouse left-clicked outside of editor space");
+                    }
+                }
+                else {
+                    SDL_Log("Mouse right-clicked");
+                }
+            }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -137,6 +189,13 @@ int main(int argc, char* argv[]) {
                 SDL_DestroyTexture(texture);
                 SDL_DestroySurface(surface);
             }
+        }
+
+        // Render cursor rectangle
+        if (showCursorRect) {
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 128);
+            SDL_RenderFillRect(renderer, &cursorRect);
+            SDL_RenderFillRect(renderer, &asciiRect);
         }
 
         SDL_RenderPresent(renderer);
