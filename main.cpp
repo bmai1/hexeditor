16fcd93b93d0a7ea0b0fceb15c1358816fe2e9da
Brian Mai <0hrs3g@gmail.com>
2026-05-28 11:12:37 -0700

    Render modified bytes in red, fix cursor states, make variable names consistent

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <unordered_set>
 
-
-std::vector<unsigned char> bytes;
+std::vector<unsigned char> bytes; // A list of bytes from input binary file
+std::unordered_set<int> edited_bytes; // A set of indices where bytes have been modified (for rendering)
 std::string file_path;
 
 // Set file selected from dialog upon opening
@@ -88,6 +89,18 @@ size_t get_byte_index(int mouse_x, int mouse_y)
     return index;
 }
 
+// Convert byte_index to hex string
+std::string index_to_hex(int byte_index) {
+    unsigned char b = bytes[byte_index];
+
+    std::ostringstream oss;
+    oss << std::hex << std::uppercase << std::setw(2)
+        << std::setfill('0')
+        << (int)b;
+
+    return oss.str();
+}
+
 bool save_file(const std::string& file_path, const std::vector<unsigned char>& bytes)
 {
     std::ofstream file(file_path, std::ios::binary);
@@ -123,23 +136,27 @@ int main(int argc, char* argv[]) {
     SDL_ShowOpenFileDialog(callback, NULL, window, NULL, 0, NULL, false);
 
     bool running = true;
-    bool showCursorRect = false;
+    bool show_cursor_rect = false;
 
-    SDL_FRect cursorRect;
-    SDL_FRect asciiRect;
+    // Highlights bytes/ASCII when selected
+    SDL_FRect cursor_rect;
+    SDL_FRect ascii_rect;
 
-    SDL_FRect saveFileRect = {0, 0, 100, 20};
-    SDL_Surface* saveFileSurface = TTF_RenderText_Solid(font, "Save File", 0, white);
-    SDL_Texture* saveFileTexture = SDL_CreateTextureFromSurface(renderer, saveFileSurface);
-    SDL_DestroySurface(saveFileSurface);
+    // Save file button in bottom-right
+    SDL_FRect save_file_rect = {0, 0, 100, 20};
+    SDL_Surface* save_file_surface = TTF_RenderText_Solid(font, "Save File", 0, white);
+    SDL_Texture* save_file_texture = SDL_CreateTextureFromSurface(renderer, save_file_surface);
+    SDL_DestroySurface(save_file_surface);
 
-    size_t byte_index;
-    size_t prev_byte_index = -1; // To track double clicks on bytes
-    bool showEditRect = false;
-    SDL_FRect editRect; 
-    std::string editByte = "00";
-    SDL_Surface* editByteSurface;
-    SDL_Texture* editByteTexture;
+    size_t byte_index = -1;
+    size_t prev_byte_index = -1;
+
+    // Displays incoming edit when double-clicking byte
+    bool show_edit_rect = false;
+    SDL_FRect edit_rect; 
+    std::string edit_byte = "00"; 
+    SDL_Surface* edit_byte_surface;
+    SDL_Texture* edit_byte_texture;
 
     while (running) {
         SDL_Delay(50); // decrease fps 
@@ -161,40 +178,41 @@ int main(int argc, char* argv[]) {
                         save_file(file_path, bytes);
                     }
                     else {
+                        // Handle highlighting bytes based on left clicks
                         byte_index = get_byte_index((int) click_x, (int) click_y);
                         
-                        if (byte_index == prev_byte_index) {
-                            if (!showEditRect) {
-                                showEditRect = true;
-                                SDL_StartTextInput(window);
-                                editRect = {(float) 110+30*(byte_index%16), (float) 30+16*(byte_index/16), 20, 16};
-                            }
-                            else {
-                                showEditRect = false;
-                                editByte = "00";
-                                SDL_StopTextInput(window);
-                            }
-                        }
-                        if (byte_index != (size_t)-1) {
-                            if (byte_index != prev_byte_index) {
-                                showEditRect = false;
-                                editByte = "00";
-                                SDL_StopTextInput(window);
-                            }
-                            SDL_Log("Mouse left-clicked at: %f, %f, which corresponds to byte index %zu", click_x, click_y, byte_index);
-                            // Calculated based on offsets in get_byte_index
-                            cursorRect = {(float) 110+30*(byte_index%16), (float) 31+16*(byte_index/16), 20, 14};
-                            asciiRect  = {(float) 600+10*(byte_index%16), (float) 31+16*(byte_index/16), 10, 14};
-                            if (!showCursorRect) {
-                                showCursorRect = true;
-                            }
-                            else {
-                                // showCursorRect = false;
-                            }
-                            prev_byte_index = byte_index;
+                        bool valid = (byte_index >= 0 && byte_index < bytes.size());
+
+                        // If invalid, reset states
+                        if (!valid) {
+                            SDL_Log("Mouse left-clicked outside of editor space");
+                            show_cursor_rect = false;
+                            show_edit_rect = false;
+                            SDL_StopTextInput(window);
                         }
                         else {
-                            SDL_Log("Mouse left-clicked outside of editor space");
+                            SDL_Log("Mouse left-clicked at: %f, %f, which corresponds to byte index %zu", click_x, click_y, byte_index);
+                            
+                            // IF IN DEFAULT OR EDIT MODE AND LEFT CLICK -> ENTER CURSOR MODE
+                            if (!show_cursor_rect) { 
+                                show_cursor_rect = true;
+                                show_edit_rect = false;
+                                SDL_StopTextInput(window);
+                            }
+                            // IF IN CURSOR MODE AND LEFT CLICK SAME BYTE -> ENTER EDIT MODE
+                            else if (byte_index == prev_byte_index) {
+                                edit_byte = index_to_hex(byte_index);
+                                show_cursor_rect = false;
+                                show_edit_rect = true;
+                                SDL_StartTextInput(window);
+                            } 
+
+                            // (If in cursor mode and left click new byte, nothing happens besides the rectangles being updated)
+                            cursor_rect = {(float) 110+30*(byte_index%16), (float) 31+16*(byte_index/16), 20, 14};
+                            edit_rect   = {(float) 110+30*(byte_index%16), (float) 30+16*(byte_index/16), 20, 16};
+                            ascii_rect  = {(float) 600+10*(byte_index%16), (float) 31+16*(byte_index/16), 10, 14};
+                            
+                            prev_byte_index = byte_index;
                         }
                     }
                 }
@@ -202,30 +220,82 @@ int main(int argc, char* argv[]) {
                     SDL_Log("Mouse right-clicked");
                 }
             }
-            else if (event.type == SDL_EVENT_TEXT_INPUT && showEditRect) {
+            // Handle keyboard input for editing bytes
+            else if (event.type == SDL_EVENT_TEXT_INPUT && show_edit_rect) {
                 SDL_Log("Typed: %s", event.text.text);
-                editByte.erase(0, 1); // pop first char
-                editByte += event.text.text; 
+                if (std::isalnum(event.text.text[0])) {
+                    if (edit_byte.size() == 2) {
+                        edit_byte = event.text.text;
+                    }
+                    else if (edit_byte.size() < 2) {
+                        edit_byte += event.text.text; 
+                    }
+                }
             }
             else if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_BACKSPACE) {
-                editByte = "0" + editByte;
-                editByte.pop_back();
-            }
-            else if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_RETURN && showEditRect) {
-                if (byte_index < bytes.size()) {
-                    bytes[byte_index] = (unsigned char) std::stoi(editByte, nullptr, 16);
+                if (edit_byte.size() > 0) {
+                    edit_byte.pop_back();
                 }
-                showEditRect = false;
-                editByte = "00";
-                SDL_StopTextInput(window);
+            }
+            // Handle keyboard inputs
+            else if (event.type == SDL_EVENT_KEY_DOWN) {
+                bool edited = false;
+
+                // Handle enter key: enter edit byte mode if cursor is active OR update the byte array (not saving to the file yet)
+                if (event.key.key == SDLK_RETURN && edit_byte.size() == 2) { // Enfore 2 char length for hex byte
+                    if (show_cursor_rect && !show_edit_rect) {
+                        show_cursor_rect = false;
+                        show_edit_rect = true;
+                        SDL_StartTextInput(window);
+                        edit_rect = {(float) 110+30*(byte_index%16), (float) 30+16*(byte_index/16), 20, 16};
+                    }
+                    else if (show_edit_rect) {
+                        if (byte_index >= 0 && byte_index < bytes.size()) {
+                            bytes[byte_index] = (unsigned char) std::stoi(edit_byte, nullptr, 16);
+                            edited_bytes.insert(byte_index); // Render edited bytes in red
+                        }
+        
+                        // Automatically move to editing next byte (re-render rects) instead of exiting edit mode
+                        if (byte_index < bytes.size() - 1) {
+                            byte_index++;
+                            edited = true;
+                        }
+                    }
+                }
+                // Handle arrow key cursor movement (note: does not update byte array)
+                else if (event.key.key == SDLK_LEFT && (show_cursor_rect || show_edit_rect) && byte_index > 0 && byte_index < bytes.size()) {
+                    byte_index--;
+                    edited = true;
+                }
+                else if (event.key.key == SDLK_RIGHT && (show_cursor_rect || show_edit_rect) && byte_index >= 0 && byte_index < bytes.size() - 1) {
+                    byte_index++;
+                    edited = true;
+                }
+
+                // Re-render cursor rectangles
+                if (byte_index != -1) {
+                    if (edited) edit_byte = index_to_hex(byte_index);
+                    edit_rect   = {(float) 110+30*(byte_index%16), (float) 30+16*(byte_index/16), 20, 16};
+                    cursor_rect = {(float) 110+30*(byte_index%16), (float) 31+16*(byte_index/16), 20, 14};
+                    ascii_rect  = {(float) 600+10*(byte_index%16), (float) 31+16*(byte_index/16), 10, 14};
+                }
             }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        if (!bytes.empty()) {
+        // Render red highlights underneath edited bytes
+        for (int edited_byte_index : edited_bytes) {
+            SDL_FRect edited_rect = {(float) 110+30*(edited_byte_index%16), (float) 31+16*(edited_byte_index/16), 20, 14};
+            SDL_FRect edited_ascii_rect  = {(float) 600+10*(edited_byte_index%16), (float) 31+16*(edited_byte_index/16), 10, 14};
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 192);
+            SDL_RenderFillRect(renderer, &edited_rect);
+            SDL_RenderFillRect(renderer, &edited_ascii_rect);
+        }
 
+        // Render bytes as lines
+        if (!bytes.empty()) {
             int line_height = 16;
             size_t total_rows = (bytes.size() + 15) / 16;
 
@@ -248,6 +318,7 @@ int main(int argc, char* argv[]) {
                 SDL_Surface* surface = TTF_RenderText_Solid(font, lp, 0, white);
                 SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 
+                // For text location/proper sizing
                 SDL_FRect dst = {
                     10.0f,
                     10.0f + row * line_height,
@@ -263,51 +334,52 @@ int main(int argc, char* argv[]) {
         }
 
         // Render cursor rectangle
-        if (showCursorRect) {
+        if (show_cursor_rect) {
             SDL_SetRenderDrawColor(renderer, 255, 255, 255, 128);
-            SDL_RenderFillRect(renderer, &cursorRect);
-            SDL_RenderFillRect(renderer, &asciiRect);
+            SDL_RenderFillRect(renderer, &cursor_rect);
+            SDL_RenderFillRect(renderer, &ascii_rect);
         }
 
         // Blue highlight when editing byte
-        if (showEditRect) {
-            SDL_SetRenderDrawColor(renderer, 0, 100, 255, 255);
-            SDL_RenderFillRect(renderer, &editRect);
+        if (show_edit_rect) {
+            SDL_SetRenderDrawColor(renderer, 60, 77, 145, 255);
+            SDL_RenderFillRect(renderer, &edit_rect);
 
             // Edited byte label
-            editByteSurface = TTF_RenderText_Solid(font, editByte.c_str(), 0, white);
-            editByteTexture = SDL_CreateTextureFromSurface(renderer, editByteSurface);
-            SDL_DestroySurface(editByteSurface);
+            edit_byte_surface = TTF_RenderText_Solid(font, edit_byte.c_str(), 0, white);
+            edit_byte_texture = SDL_CreateTextureFromSurface(renderer, edit_byte_surface);
+            SDL_DestroySurface(edit_byte_surface);
             SDL_FRect dst = {
-                editRect.x,
-                editRect.y - 4,
-                20,
+                edit_rect.x,
+                edit_rect.y - 4,
+                10 * (float) edit_byte.size(), // So the numbers don't change width when typing
                 24,
             };
-            SDL_RenderTexture(renderer, editByteTexture, NULL, &dst);
+            SDL_RenderTexture(renderer, edit_byte_texture, NULL, &dst);
         }
 
         // Save button
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 64);
-        saveFileRect.x = width - 120;
-        saveFileRect.y = height - 40;
-        SDL_RenderFillRect(renderer, &saveFileRect);
+        save_file_rect.x = width - 120;
+        save_file_rect.y = height - 40;
+        SDL_RenderFillRect(renderer, &save_file_rect);
 
         // Save button label
-        SDL_FRect dst = { saveFileRect.x + 10, saveFileRect.y - 3, 80, 24 };
-        SDL_RenderTexture(renderer, saveFileTexture, NULL, &dst);
+        SDL_FRect dst = { save_file_rect.x + 10, save_file_rect.y - 3, 80, 24 };
+        SDL_RenderTexture(renderer, save_file_texture, NULL, &dst);
 
         SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyTexture(editByteTexture);
-    SDL_DestroyTexture(saveFileTexture);
+    SDL_DestroyTexture(edit_byte_texture);
+    SDL_DestroyTexture(save_file_texture);
 
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     TTF_Quit();
     SDL_Quit();
+
     return 0;
 }
 
